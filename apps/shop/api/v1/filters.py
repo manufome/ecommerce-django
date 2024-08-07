5468c55cfe5370e3b10063cbe3c57ab6f59d5250
manufome <manuelito100810@gmail.com>
2024-08-07 11:44:04 -0500

    Refactor product filtering logic and add price range filters

--- apps/shop/api/v1/filters.py
+++ apps/shop/api/v1/filters.py
@@ -2,16 +2,17 @@
 from apps.shop.models import Product, Category
 from django.shortcuts import get_object_or_404
 from django_filters import rest_framework as filters
+from django.db.models import F, ExpressionWrapper, DecimalField
 
 class ProductFilter(filters.FilterSet):
     category = filters.CharFilter(field_name='category__slug', method='filter_by_category')
     brands = filters.CharFilter(field_name='brand__slug', method='filter_by_brands')
-    min_price = filters.NumberFilter(field_name="price", lookup_expr='gte')
-    max_price = filters.NumberFilter(field_name="price", lookup_expr='lte')
+    min_price = filters.NumberFilter(field_name="price", method='filter_by_min_price')
+    max_price = filters.NumberFilter(field_name="price", method='filter_by_max_price')
 
     class Meta:
         model = Product
-        fields = ['category', 'brand', 'is_new', 'is_top', 'is_featured']
+        fields = ['is_new', 'is_top', 'is_featured']
 
     def filter_by_category(self, queryset, name, value):
         category = get_object_or_404(Category, slug=value)
@@ -21,3 +22,21 @@ class ProductFilter(filters.FilterSet):
     def filter_by_brands(self, queryset, name, value):
         brands = value.split(',')
         return queryset.filter(brand__slug__in=brands)
+    
+    def filter_by_min_price(self, queryset, name, value):
+        queryset = queryset.annotate(
+            discount_price=ExpressionWrapper(
+                F('price') - (F('price') * F('discount') / 100),
+                output_field=DecimalField()
+            )
+        )
+        return queryset.filter(discount_price__gte=value)
+    
+    def filter_by_max_price(self, queryset, name, value):
+        queryset = queryset.annotate(
+            discount_price=ExpressionWrapper(
+                F('price') - (F('price') * F('discount') / 100),
+                output_field=DecimalField()
+            )
+        )
+        return queryset.filter(discount_price__lte=value)
